b349a2590bca0b4bd4443919cc817cf889629b45
Andrew Heiss <andrew@andrewheiss.com>
2017-01-10 13:37:19 -0500

    Move pandoc settings to their own file

---
+++ lib/pandoc.R
@@ -0,0 +1,7 @@
+library(pander)
+
+panderOptions('table.split.table', Inf)
+panderOptions('table.split.cells', Inf)
+panderOptions('keep.line.breaks', TRUE)
+panderOptions('table.style', 'multiline')
+panderOptions('table.alignment.default', 'left')
