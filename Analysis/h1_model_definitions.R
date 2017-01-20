ab0934a8883302936a56231a2c22a9bb778c483d
Andrew Heiss <andrew@andrewheiss.com>
2017-01-19 22:11:49 -0500

    Use more dynamic dependent variable

--- Analysis/h1_model_definitions.R
+++ Analysis/h1_model_definitions.R
@@ -1,71 +1,66 @@
-mod.h1.barriers.total <- function(df) {
-  lmer(
-    total.oda_log_next_year ~ 
-      barriers.total_between + barriers.total_within +
-      polity_between + polity_within + 
-      gdp.capita_log_between + gdp.capita_log_within +
-      trade.pct.gdp_between + trade.pct.gdp_within + 
-      corruption_between + corruption_within +
-      # Indicator variables don't need to be demeaned
-      internal.conflict.past.5 + natural_disaster.occurrence +
-      (1 | cowcode) + (1 | year),
-    data=df)
+mod.h1.barriers.total <- function(df, outcome) {
+  indep.vars <- ~ 
+    barriers.total_between + barriers.total_within +
+    polity_between + polity_within + 
+    gdp.capita_log_between + gdp.capita_log_within +
+    trade.pct.gdp_between + trade.pct.gdp_within + 
+    corruption_between + corruption_within +
+    internal.conflict.past.5 + natural_disaster.occurrence +
+    (1 | cowcode) + (1 | year)
+  
+  lmer(build.formula(outcome, indep.vars), data=df)
 }
 
-mod.h1.barriers.new <- function(df) {
-  lmer(
-    total.oda_log_next_year ~ 
-      barriers.total_new +
-      polity_between + polity_within + 
-      gdp.capita_log_between + gdp.capita_log_within +
-      trade.pct.gdp_between + trade.pct.gdp_within + 
-      corruption_between + corruption_within +
-      # Indicator variables don't need to be demeaned
-      internal.conflict.past.5 + natural_disaster.occurrence +
-      (1 | cowcode) + (1 | year),
-    data=df)
+mod.h1.barriers.new <- function(df, outcome) {
+  indep.vars <- ~ 
+    barriers.total_new +
+    polity_between + polity_within + 
+    gdp.capita_log_between + gdp.capita_log_within +
+    trade.pct.gdp_between + trade.pct.gdp_within + 
+    corruption_between + corruption_within +
+    internal.conflict.past.5 + natural_disaster.occurrence +
+    (1 | cowcode) + (1 | year)
+  
+  lmer(build.formula(outcome, indep.vars), data=df)
 }
 
-mod.h1.type.total <- function(df) {
-  lmer(
-    total.oda_log_next_year ~ 
-      advocacy_between + advocacy_within +
-      entry_between + entry_within +
-      funding_between + funding_within +
-      polity_between + polity_within + 
-      gdp.capita_log_between + gdp.capita_log_within +
-      trade.pct.gdp_between + trade.pct.gdp_within + 
-      corruption_between + corruption_within +
-      # Indicator variables don't need to be demeaned
-      internal.conflict.past.5 + natural_disaster.occurrence +
-      (1 | cowcode) + (1 | year),
-    data=df)
+mod.h1.type.total <- function(df, outcome) {
+  indep.vars <- ~ 
+    advocacy_between + advocacy_within +
+    entry_between + entry_within +
+    funding_between + funding_within +
+    polity_between + polity_within + 
+    gdp.capita_log_between + gdp.capita_log_within +
+    trade.pct.gdp_between + trade.pct.gdp_within + 
+    corruption_between + corruption_within +
+    internal.conflict.past.5 + natural_disaster.occurrence +
+    (1 | cowcode) + (1 | year)
+  
+  lmer(build.formula(outcome, indep.vars), data=df)
 }
 
-mod.h1.type.new <- function(df) {
-  lmer(
-    total.oda_log_next_year ~ 
-      advocacy_new + entry_new + funding_new +
-      polity_between + polity_within + 
-      gdp.capita_log_between + gdp.capita_log_within +
-      trade.pct.gdp_between + trade.pct.gdp_within + 
-      corruption_between + corruption_within +
-      # Indicator variables don't need to be demeaned
-      internal.conflict.past.5 + natural_disaster.occurrence +
-      (1 | cowcode) + (1 | year),
-    data=df)
+mod.h1.type.new <- function(df, outcome) {
+  indep.vars <- ~ 
+    advocacy_new + entry_new + funding_new +
+    polity_between + polity_within + 
+    gdp.capita_log_between + gdp.capita_log_within +
+    trade.pct.gdp_between + trade.pct.gdp_within + 
+    corruption_between + corruption_within +
+    internal.conflict.past.5 + natural_disaster.occurrence +
+    (1 | cowcode) + (1 | year)
+  
+  lmer(build.formula(outcome, indep.vars), data=df)
 }
 
-mod.h1.csre <- function(df) {
-  lmer(
-    total.oda_log_next_year ~ 
-      csre_between + csre_within +
-      polity_between + polity_within + 
-      gdp.capita_log_between + gdp.capita_log_within +
-      trade.pct.gdp_between + trade.pct.gdp_within + 
-      corruption_between + corruption_within +
-      # Indicator variables don't need to be demeaned
-      internal.conflict.past.5 + natural_disaster.occurrence +
-      (1 | cowcode) + (1 | year),
-    data=df)
+mod.h1.csre <- function(df, outcome) {
+  indep.vars <- ~ 
+    csre_between + csre_within +
+    polity_between + polity_within + 
+    gdp.capita_log_between + gdp.capita_log_within +
+    trade.pct.gdp_between + trade.pct.gdp_within + 
+    corruption_between + corruption_within +
+    internal.conflict.past.5 + natural_disaster.occurrence +
+    (1 | cowcode) + (1 | year)
+  
+  lmer(build.formula(outcome, indep.vars), data=df)
 }
