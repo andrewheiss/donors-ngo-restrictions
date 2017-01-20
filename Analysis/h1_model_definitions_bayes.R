5b07f04d25ac28d4e25265d89d0de10fd0da6fca
Andrew Heiss <andrew@andrewheiss.com>
2017-01-19 22:48:24 -0500

    Add updated Bayesian models

--- Analysis/h1_model_definitions_bayes.R
+++ Analysis/h1_model_definitions_bayes.R
@@ -1,81 +1,81 @@
-mod.h1.barriers.total.bayes <- function(df) {
-  stan_glmer(
-    total.oda_log_next_year ~ 
-      barriers.total_between + barriers.total_within +
-      polity_between + polity_within + 
-      gdp.capita_log_between + gdp.capita_log_within +
-      trade.pct.gdp_between + trade.pct.gdp_within + 
-      corruption_between + corruption_within +
-      # Indicator variables don't need to be demeaned
-      internal.conflict.past.5 + natural_disaster.occurrence +
-      (1 | cowcode) + (1 | year),
-    data=df, family=gaussian(),
-    prior=normal(), prior_intercept=normal(),
-    chains=CHAINS, iter=ITER, warmup=WARMUP, seed=BAYES.SEED)
+mod.h1.barriers.total.bayes <- function(df, outcome) {
+  indep.vars <- ~ 
+    barriers.total_between + barriers.total_within +
+    polity_between + polity_within + 
+    gdp.capita_log_between + gdp.capita_log_within +
+    trade.pct.gdp_between + trade.pct.gdp_within + 
+    corruption_between + corruption_within +
+    internal.conflict.past.5 + natural_disaster.occurrence +
+    (1 | cowcode) + (1 | year)
+  
+  stan_glmer(build.formula(outcome, indep.vars),
+             data=df, family=gaussian(),
+             prior=normal(), prior_intercept=normal(),
+             chains=CHAINS, iter=ITER, warmup=WARMUP, seed=BAYES.SEED)
 }
 
-mod.h1.barriers.new.bayes <- function(df) {
-  stan_glmer(
-    total.oda_log_next_year ~ 
-      barriers.total_new +
-      polity_between + polity_within + 
-      gdp.capita_log_between + gdp.capita_log_within +
-      trade.pct.gdp_between + trade.pct.gdp_within + 
-      corruption_between + corruption_within +
-      # Indicator variables don't need to be demeaned
-      internal.conflict.past.5 + natural_disaster.occurrence +
-      (1 | cowcode) + (1 | year),
-    data=df, family=gaussian(),
-    prior=normal(), prior_intercept=normal(),
-    chains=CHAINS, iter=ITER, warmup=WARMUP, seed=BAYES.SEED)
+mod.h1.barriers.new.bayes <- function(df, outcome) {
+  indep.vars <- ~ 
+    barriers.total_new +
+    polity_between + polity_within + 
+    gdp.capita_log_between + gdp.capita_log_within +
+    trade.pct.gdp_between + trade.pct.gdp_within + 
+    corruption_between + corruption_within +
+    internal.conflict.past.5 + natural_disaster.occurrence +
+    (1 | cowcode) + (1 | year)
+  
+  stan_glmer(build.formula(outcome, indep.vars),
+             data=df, family=gaussian(),
+             prior=normal(), prior_intercept=normal(),
+             chains=CHAINS, iter=ITER, warmup=WARMUP, seed=BAYES.SEED)
 }
 
-mod.h1.type.total.bayes <- function(df) {
-  stan_glmer(
-    total.oda_log_next_year ~ 
-      advocacy_between + advocacy_within +
-      entry_between + entry_within +
-      funding_between + funding_within +
-      polity_between + polity_within + 
-      gdp.capita_log_between + gdp.capita_log_within +
-      trade.pct.gdp_between + trade.pct.gdp_within + 
-      corruption_between + corruption_within +
-      # Indicator variables don't need to be demeaned
-      internal.conflict.past.5 + natural_disaster.occurrence +
-      (1 | cowcode) + (1 | year),
-    data=df, family=gaussian(),
-    prior=normal(), prior_intercept=normal(),
-    chains=CHAINS, iter=ITER, warmup=WARMUP, seed=BAYES.SEED)
+mod.h1.type.total.bayes <- function(df, outcome) {
+  indep.vars <- ~ 
+    advocacy_between + advocacy_within +
+    entry_between + entry_within +
+    funding_between + funding_within +
+    polity_between + polity_within + 
+    gdp.capita_log_between + gdp.capita_log_within +
+    trade.pct.gdp_between + trade.pct.gdp_within + 
+    corruption_between + corruption_within +
+    internal.conflict.past.5 + natural_disaster.occurrence +
+    (1 | cowcode) + (1 | year)
+  
+  stan_glmer(build.formula(outcome, indep.vars),
+             data=df, family=gaussian(),
+             prior=normal(), prior_intercept=normal(),
+             chains=CHAINS, iter=ITER, warmup=WARMUP, seed=BAYES.SEED)
 }
 
-mod.h1.type.new.bayes <- function(df) {
-  stan_glmer(
-    total.oda_log_next_year ~ 
-      advocacy_new + entry_new + funding_new +
-      polity_between + polity_within + 
-      gdp.capita_log_between + gdp.capita_log_within +
-      trade.pct.gdp_between + trade.pct.gdp_within + 
-      corruption_between + corruption_within +
-      # Indicator variables don't need to be demeaned
-      internal.conflict.past.5 + natural_disaster.occurrence +
-      (1 | cowcode) + (1 | year),
-    data=df, family=gaussian(),
-    prior=normal(), prior_intercept=normal(),
-    chains=CHAINS, iter=ITER, warmup=WARMUP, seed=BAYES.SEED)
+mod.h1.type.new.bayes <- function(df, outcome) {
+  indep.vars <- ~ 
+    advocacy_new + entry_new + funding_new +
+    polity_between + polity_within + 
+    gdp.capita_log_between + gdp.capita_log_within +
+    trade.pct.gdp_between + trade.pct.gdp_within + 
+    corruption_between + corruption_within +
+    internal.conflict.past.5 + natural_disaster.occurrence +
+    (1 | cowcode) + (1 | year)
+  
+  stan_glmer(build.formula(outcome, indep.vars),
+             data=df, family=gaussian(),
+             prior=normal(), prior_intercept=normal(),
+             chains=CHAINS, iter=ITER, warmup=WARMUP, seed=BAYES.SEED)
 }
 
-mod.h1.csre.bayes <- function(df) {
-  stan_glmer(
-    total.oda_log_next_year ~ 
-      csre_between + csre_within +
-      polity_between + polity_within + 
-      gdp.capita_log_between + gdp.capita_log_within +
-      trade.pct.gdp_between + trade.pct.gdp_within + 
-      corruption_between + corruption_within +
-      # Indicator variables don't need to be demeaned
-      internal.conflict.past.5 + natural_disaster.occurrence +
-      (1 | cowcode) + (1 | year),
-    data=df, family=gaussian(),
-    prior=normal(), prior_intercept=normal(),
-    chains=CHAINS, iter=ITER, warmup=WARMUP, seed=BAYES.SEED)
+mod.h1.csre.bayes <- function(df, outcome) {
+  indep.vars <- ~ 
+    csre_between + csre_within +
+    polity_between + polity_within + 
+    gdp.capita_log_between + gdp.capita_log_within +
+    trade.pct.gdp_between + trade.pct.gdp_within + 
+    corruption_between + corruption_within +
+    internal.conflict.past.5 + natural_disaster.occurrence +
+    (1 | cowcode) + (1 | year)
+  
+  stan_glmer(build.formula(outcome, indep.vars),
+             data=df, family=gaussian(),
+             prior=normal(), prior_intercept=normal(),
+             chains=CHAINS, iter=ITER, warmup=WARMUP, seed=BAYES.SEED)
 }
