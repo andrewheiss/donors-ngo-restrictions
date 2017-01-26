15ee14fe36f6dbe3a2d8daa1460e4c51b4ccc44d
Andrew Heiss <andrew@andrewheiss.com>
2017-01-26 15:25:49 -0500

    Handle degenerate models with nonsplit current ODA

--- Analysis/h1_model_definitions.R
+++ Analysis/h1_model_definitions.R
@@ -3,9 +3,9 @@ mod.h1.barriers.total <- function(df, outcome) {
     barriers.total_within + barriers.total_between +
     polity_within + polity_between +
     gdp.capita_log_within + gdp.capita_log_between +
-    total.oda_log_within + total.oda_log_between +
     trade.pct.gdp_within + trade.pct.gdp_between +
     corruption_within + corruption_between +
+    total.oda_log +
     internal.conflict.past.5 + natural_disaster.occurrence + post.1989 +
     (1 | cowcode) + (1 | year)
   
@@ -17,9 +17,9 @@ mod.h1.barriers.new <- function(df, outcome) {
     barriers.total_new +
     polity_within + polity_between + 
     gdp.capita_log_within + gdp.capita_log_between +
-    total.oda_log_within + total.oda_log_between +
     trade.pct.gdp_within + trade.pct.gdp_between +
     corruption_within + corruption_between +
+    total.oda_log +
     internal.conflict.past.5 + natural_disaster.occurrence + post.1989 +
     (1 | cowcode) + (1 | year)
   
@@ -33,9 +33,9 @@ mod.h1.type.total <- function(df, outcome) {
     funding_within + funding_between +
     polity_within + polity_between + 
     gdp.capita_log_within + gdp.capita_log_between +
-    total.oda_log_within + total.oda_log_between +
     trade.pct.gdp_within + trade.pct.gdp_between +
     corruption_within + corruption_between +
+    total.oda_log +
     internal.conflict.past.5 + natural_disaster.occurrence + post.1989 +
     (1 | cowcode) + (1 | year)
   
@@ -47,9 +47,9 @@ mod.h1.type.new <- function(df, outcome) {
     advocacy_new + entry_new + funding_new +
     polity_within + polity_between + 
     gdp.capita_log_within + gdp.capita_log_between +
-    total.oda_log_within + total.oda_log_between +
     trade.pct.gdp_within + trade.pct.gdp_between +
     corruption_within + corruption_between +
+    total.oda_log +
     internal.conflict.past.5 + natural_disaster.occurrence + post.1989 +
     (1 | cowcode) + (1 | year)
   
@@ -61,9 +61,9 @@ mod.h1.csre <- function(df, outcome) {
     csre_within + csre_between +
     polity_within + polity_between + 
     gdp.capita_log_within + gdp.capita_log_between +
-    total.oda_log_within + total.oda_log_between +
     trade.pct.gdp_within + trade.pct.gdp_between +
     corruption_within + corruption_between +
+    total.oda_log +
     internal.conflict.past.5 + natural_disaster.occurrence + post.1989 +
     (1 | cowcode) + (1 | year)
   
