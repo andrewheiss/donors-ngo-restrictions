6b3247e22695e00be2189920e5b89062d6460402
Andrew Heiss <andrew@andrewheiss.com>
2017-04-25 20:48:46 -0400

    Add robustness checks for neutral regulations + move frequentist models out

--- Analysis/robustness_models_definitions.R
+++ Analysis/robustness_models_definitions.R
@@ -1,37 +1,108 @@
-mod.h1.advocacy.total <- function(df, outcome) {
+mod.h1.neutral.reg <- function(df, outcome) {
   indep.vars <- ~ 
-    advocacy_between + advocacy_within +
-    polity_between + polity_within + 
-    gdp.capita_log_between + gdp.capita_log_within +
-    trade.pct.gdp_between + trade.pct.gdp_within + 
+    ngo_register +
+    gdp.capita_log_within + gdp.capita_log_between +
+    trade.pct.gdp_within + trade.pct.gdp_between +
     corruption_within + corruption_between +
+    total.oda_log +
     internal.conflict.past.5 + natural_disaster.occurrence + post.1989 +
     (1 | cowcode) + (1 | year)
   
   lmer(build.formula(outcome, indep.vars), data=df)
 }
 
-mod.h1.entry.total <- function(df, outcome) {
+mod.h1.neutral.fund <- function(df, outcome) {
   indep.vars <- ~ 
-    entry_between + entry_within +
-    polity_between + polity_within + 
-    gdp.capita_log_between + gdp.capita_log_within +
-    trade.pct.gdp_between + trade.pct.gdp_within + 
+    ngo_disclose_funds +
+    gdp.capita_log_within + gdp.capita_log_between +
+    trade.pct.gdp_within + trade.pct.gdp_between +
     corruption_within + corruption_between +
+    total.oda_log +
     internal.conflict.past.5 + natural_disaster.occurrence + post.1989 +
     (1 | cowcode) + (1 | year)
   
   lmer(build.formula(outcome, indep.vars), data=df)
 }
 
-mod.h1.funding.total <- function(df, outcome) {
+mod.h2.neutral.reg <- function(df, outcome) {
   indep.vars <- ~ 
-    funding_between + funding_within +
-    polity_between + polity_within + 
-    gdp.capita_log_between + gdp.capita_log_within +
-    trade.pct.gdp_between + trade.pct.gdp_within + 
+    ngo_register +
+    polity_within + polity_between +
+    gdp.capita_log_within + gdp.capita_log_between +
+    trade.pct.gdp_within + trade.pct.gdp_between +
     corruption_within + corruption_between +
+    prop.contentious_logit +
     internal.conflict.past.5 + natural_disaster.occurrence + post.1989 +
+    (1 | cowcode) + (1 | year)
+  
+  lmer(build.formula(outcome, indep.vars), data=df)
+}
+
+mod.h2.neutral.fund <- function(df, outcome) {
+  indep.vars <- ~ 
+    ngo_disclose_funds +
+    polity_within + polity_between +
+    gdp.capita_log_within + gdp.capita_log_between +
+    trade.pct.gdp_within + trade.pct.gdp_between +
+    corruption_within + corruption_between +
+    prop.contentious_logit +
+    internal.conflict.past.5 + natural_disaster.occurrence + post.1989 +
+    (1 | cowcode) + (1 | year)
+  
+  lmer(build.formula(outcome, indep.vars), data=df)
+}
+
+mod.h3.dom.neutral.reg <- function(df, outcome) {
+  indep.vars <- ~ 
+    ngo_register +
+    polity_within + polity_between +
+    gdp.capita_log_within + gdp.capita_log_between +
+    trade.pct.gdp_within + trade.pct.gdp_between +
+    corruption_within + corruption_between +
+    prop.ngo.dom_logit +
+    internal.conflict.past.5 + natural_disaster.occurrence +
+    (1 | cowcode) + (1 | year)
+  
+  lmer(build.formula(outcome, indep.vars), data=df)
+}
+
+mod.h3.dom.neutral.fund <- function(df, outcome) {
+  indep.vars <- ~ 
+    ngo_disclose_funds +
+    polity_within + polity_between +
+    gdp.capita_log_within + gdp.capita_log_between +
+    trade.pct.gdp_within + trade.pct.gdp_between +
+    corruption_within + corruption_between +
+    prop.ngo.dom_logit +
+    internal.conflict.past.5 + natural_disaster.occurrence +
+    (1 | cowcode) + (1 | year)
+  
+  lmer(build.formula(outcome, indep.vars), data=df)
+}
+
+mod.h3.for.neutral.reg <- function(df, outcome) {
+  indep.vars <- ~ 
+    ngo_register +
+    polity_within + polity_between +
+    gdp.capita_log_within + gdp.capita_log_between +
+    trade.pct.gdp_within + trade.pct.gdp_between +
+    corruption_within + corruption_between +
+    prop.ngo.foreign_logit +
+    internal.conflict.past.5 + natural_disaster.occurrence +
+    (1 | cowcode) + (1 | year)
+  
+  lmer(build.formula(outcome, indep.vars), data=df)
+}
+
+mod.h3.for.neutral.fund <- function(df, outcome) {
+  indep.vars <- ~ 
+    ngo_disclose_funds +
+    polity_within + polity_between +
+    gdp.capita_log_within + gdp.capita_log_between +
+    trade.pct.gdp_within + trade.pct.gdp_between +
+    corruption_within + corruption_between +
+    prop.ngo.foreign_logit +
+    internal.conflict.past.5 + natural_disaster.occurrence +
     (1 | cowcode) + (1 | year)
   
   lmer(build.formula(outcome, indep.vars), data=df)
