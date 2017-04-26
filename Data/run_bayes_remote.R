27716ab9cd338e23cd0c5d0273c147ea6ca11c0b
Andrew Heiss <andrew@andrewheiss.com>
2017-04-25 22:02:36 -0400

    Move heavy lifting of rstanarm processing to separate file

---
+++ Data/run_bayes_remote.R
@@ -0,0 +1,105 @@
+# devtools::install_github("robertzk/s3mpi@0.2.31")
+# nohup Rscript run_bayes.R > bayes.log 2>&1 &
+print("Loading data")
+source(file.path(PROJHOME, "Analysis", "models-chunks.R"))
+
+source(file.path(PROJHOME, "Analysis", "h1_model_definitions_bayes.R"))
+source(file.path(PROJHOME, "Analysis", "h2_model_definitions_bayes.R"))
+source(file.path(PROJHOME, "Analysis", "h3_model_definitions_bayes.R"))
+source(file.path(PROJHOME, "Analysis", "h4_model_definitions_bayes.R"))
+
+
+# H1 ---------------------------------------------------------------------
+print("Running H1 models")
+h1.raw.path <- file.path(PROJHOME, "Data", "data_cache", "models_bayes_h1.rds")
+
+if (file.exists(h1.raw.path)) {
+  mods.h1.next_year.raw.bayes <- readRDS(h1.raw.path)
+} else {
+  mods.h1.next_year.raw.bayes <- df.country.aid.demean.next_year.both %>%
+    nest(-m) %>%
+    mutate(mod.h1.barriers.total = data %>% map(mod.h1.barriers.total.bayes,
+                                                "total.oda_log_next_year"),
+           mod.h1.barriers.new = data %>% map(mod.h1.barriers.new.bayes,
+                                              "total.oda_log_next_year"),
+           mod.h1.type.total = data %>% map(mod.h1.type.total.bayes,
+                                            "total.oda_log_next_year"),
+           mod.h1.type.new = data %>% map(mod.h1.type.new.bayes,
+                                          "total.oda_log_next_year"),
+           mod.h1.csre = data %>% map(mod.h1.csre.bayes,
+                                      "total.oda_log_next_year"))
+  
+  saveRDS(mods.h1.next_year.raw.bayes, h1.raw.path)
+}
+
+h2.raw.path <- file.path(PROJHOME, "Data", "data_cache", "models_bayes_h2.rds")
+
+
+# H2 ---------------------------------------------------------------------
+print("Running H2 models")
+if (file.exists(h2.raw.path)) {
+  mods.h2.next_year.raw.bayes <- readRDS(h2.raw.path)
+} else {
+  mods.h2.next_year.raw.bayes <- df.country.aid.demean.next_year.both %>%
+    nest(-m) %>%
+    mutate(mod.h2.barriers.total = data %>% map(mod.h2.barriers.total.bayes,
+                                                "prop.contentious_logit_next_year"),
+           mod.h2.barriers.new = data %>% map(mod.h2.barriers.new.bayes,
+                                              "prop.contentious_logit_next_year"),
+           mod.h2.type.total = data %>% map(mod.h2.type.total.bayes,
+                                            "prop.contentious_logit_next_year"),
+           mod.h2.type.new = data %>% map(mod.h2.type.new.bayes,
+                                          "prop.contentious_logit_next_year"),
+           mod.h2.csre = data %>% map(mod.h2.csre.bayes,
+                                      "prop.contentious_logit_next_year"))
+  
+  saveRDS(mods.h2.next_year.raw.bayes, h2.raw.path)
+}
+
+
+# H3 ---------------------------------------------------------------------
+print("Running H3 domestic models")
+h3.domestic.raw.path <- file.path(PROJHOME, "Data", "data_cache",
+                                  "models_bayes_h3_domestic.rds")
+
+if (file.exists(h3.domestic.raw.path)) {
+  mods.h3.dom.next_year.raw <- readRDS(h3.domestic.raw.path)
+} else {
+  mods.h3.dom.next_year.raw <- df.country.aid.us.demean.next_year.both %>%
+    nest(-m) %>%
+    mutate(mod.h3.barriers.total = data %>% map(mod.h3.barriers.total.bayes,
+                                                "prop.ngo.dom_logit_next_year"),
+           mod.h3.barriers.new = data %>% map(mod.h3.barriers.new.bayes,
+                                              "prop.ngo.dom_logit_next_year"),
+           mod.h3.type.total = data %>% map(mod.h3.type.total.bayes,
+                                            "prop.ngo.dom_logit_next_year"),
+           mod.h3.type.new = data %>% map(mod.h3.type.new.bayes,
+                                          "prop.ngo.dom_logit_next_year"),
+           mod.h3.csre = data %>% map(mod.h3.csre.bayes,
+                                      "prop.ngo.dom_logit_next_year"))
+  
+  saveRDS(mods.h3.dom.next_year.raw, h3.domestic.raw.path)
+}
+
+print("Running H3 foreign models")
+h3.foreign.raw.path <- file.path(PROJHOME, "Data", "data_cache",
+                                 "models_bayes_h3_foreign.rds")
+
+if (file.exists(h3.foreign.raw.path)) {
+  mods.h3.foreign.next_year.raw <- readRDS(h3.foreign.raw.path)
+} else {
+  mods.h3.foreign.next_year.raw <- df.country.aid.us.demean.next_year.both %>%
+    nest(-m) %>%
+    mutate(mod.h3.barriers.total = data %>% map(mod.h3.foreign.barriers.total.bayes,
+                                                "prop.ngo.foreign_logit_next_year"),
+           mod.h3.barriers.new = data %>% map(mod.h3.foreign.barriers.new.bayes,
+                                              "prop.ngo.foreign_logit_next_year"),
+           mod.h3.type.total = data %>% map(mod.h3.foreign.type.total.bayes,
+                                            "prop.ngo.foreign_logit_next_year"),
+           mod.h3.type.new = data %>% map(mod.h3.foreign.type.new.bayes,
+                                          "prop.ngo.foreign_logit_next_year"),
+           mod.h3.csre = data %>% map(mod.h3.foreign.csre.bayes,
+                                      "prop.ngo.foreign_logit_next_year"))
+  
+  saveRDS(mods.h3.foreign.next_year.raw, h3.foreign.raw.path)
+}
