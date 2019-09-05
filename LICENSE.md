03edd56c9afed2e2f0cdfe0ffc303c6c00bab76a
Andrew Heiss <andrew@andrewheiss.com>
2019-09-04 20:00:45 -0600

    Compendiumization; go to 7fa01b3 for previous history
    The analysis files and manual data files moved as a result of making this a reproducible compendium. Since GitHub doesn't use --follow in their logs, the history is hidden. Going to 7fa01b3 and seeing the history of /analysis_stuff.Rmd and Data/data_manual/* will show the whole past history

--- LICENSE.md
+++ LICENSE.md
@@ -1,6 +1,6 @@
-MIT License
+# MIT License
 
-Copyright (c) 2016 Suparna Chaudhry and Andrew Heiss
+Copyright (c) 2019 
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
---
+++ R/crackdownsphilanthropy-package.R
@@ -0,0 +1,17 @@
+#' The 'crackdownsphilanthropy' package.
+#' 
+#' @description Research on how anti-NGO crackdowns influence donor behavior
+#' 
+#' @docType package
+#' @name crackdownsphilanthropy-package
+#' @aliases crackdownsphilanthropy
+#' @useDynLib crackdownsphilanthropy, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @import rstantools
+#' @importFrom rstan sampling
+#' 
+#' @references 
+#' Stan Development Team (2018). RStan: the R interface to Stan. R package version 2.18.2. http://mc-stan.org
+#' 
+NULL
